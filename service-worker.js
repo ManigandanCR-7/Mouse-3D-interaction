14d456aaf1aa47196215f580e59ca352a43a8c7c
ManigandanCR-7 <aaron.7arun@gmail.com>
2025-02-16 19:24:25 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,10 @@ self.addEventListener("install", (event) => {
       return cache.addAll([
         "/",
         "/index.html",
+        "landing page/Cursor.html",
+        "landing page/bluerings.html",
+        "landing page/feather.html",
+        "landing page/fluid.html",
         "Frame 1.png"
         "Frame 2.png"
       ]);
