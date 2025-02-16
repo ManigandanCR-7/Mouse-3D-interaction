64459b91c4c15d0115f4580f3192114b60444411
ManigandanCR-7 <aaron.7arun@gmail.com>
2025-02-16 19:03:17 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "Frame 1.png"
+        "Frame 2.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
